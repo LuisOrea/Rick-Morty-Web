746e590eebf6e5f0cf54dfddd652ecc0e68891e6
WichoDev <luisadrianmartinezorea@gmail.com>
2024-07-12 08:30:37 -0600

    Proyecto Terminado

---
+++ src/Rutas.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FC } from "react";
+import Presentacion from "./Pages/Presentacion/Presentacion";
+import Login from "./Pages/Login/Login";
+import Home from "./Pages/Home/Home";
+
+// Componente de Rutas que define las rutas de la aplicación
+
+const Rutas: FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Presentacion />} />
+        <Route path="/Pages/Login" element={<Login />} />
+        <Route path="/Pages/Home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Rutas;
